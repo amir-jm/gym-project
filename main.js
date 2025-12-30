5655bd9b4ddc663ef13e248c106e021c2fb7dd79
amir <amirjalayeri2@gmail.com>
2025-12-30 07:36:46 -0800

    create award section

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 let navButton = document.querySelector('.nav-button');
 let navbar = document.querySelector('.myNav')
+const rightAnim = document.querySelector('#right-anim')
+const leftAnim = document.querySelector('#left-anim')
 
 navButton.addEventListener('click',function(){
     navButton.classList.toggle('change')
@@ -12,5 +14,16 @@ window.addEventListener('scroll',function(){
     }
     else{
         navbar.classList.remove('custom-navbar')
+    }
+},false)
+
+window.addEventListener('scroll',function(){
+    let position = this.scrollY
+    if(position >= 700){
+        rightAnim.classList.add('right-anim')
+        leftAnim.classList.add('left-anim')
+    }else{
+        rightAnim.classList.remove('right-anim')
+        leftAnim.classList.remove('left-anim')
     }
 },false)
