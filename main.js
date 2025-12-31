4693f7da0a2d92de929f3e240fd4b7be2c9cba7a
amir <amirjalayeri2@gmail.com>
2025-12-31 05:55:05 -0800

    create gallery section

--- main.js
+++ main.js
@@ -27,3 +27,27 @@ window.addEventListener('scroll',function(){
         leftAnim.classList.remove('left-anim')
     }
 },false)
+
+/////////////gallery///////////////////
+document.querySelectorAll('.category-link').forEach(item =>{
+    item.addEventListener('click',function(){
+        let value = this.getAttribute('data-filter')
+        this.classList.add('active-item')
+        document.querySelectorAll('.category-link').forEach(sibling =>{
+            if(sibling !== this){
+                sibling.classList.remove('active-item')
+            }
+        })
+        document.querySelectorAll('#filter').forEach(el=>{
+            if(value === 'all'){
+                el.style.display = 'block' 
+            }
+            else if(el.classList.contains(value)){
+                el.style.display = 'block'
+            }else{
+                el.style.display = 'none'
+            }
+        })
+    },false)
+})
+/////////////end of gallery///////////////////
